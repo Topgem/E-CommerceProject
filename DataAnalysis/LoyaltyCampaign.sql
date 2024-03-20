fe86ed457f79dc881a9a3055462c92b9f87daaba
Olatunji Toba <Olatunjitoba4@gmail.com>
2024-03-20 12:48:23 +0000

    I modified the Loyalty Campaign queries

--- DataAnalysis/LoyaltyCampaign.sql
+++ DataAnalysis/LoyaltyCampaign.sql
@@ -1,39 +1,14 @@
--- Loyalty Campaign
-
-WITH customer_revenue AS (
-    SELECT
-        u.id AS customer_id,
-        u.first_name,
-        u.last_name,
-        u.gender,
-        p.brand,
-        u.country,
-        SUM(oi.sale_price * o.num_of_item) AS total_revenue
-    FROM
-        users u
-    JOIN
-        orders o ON u.id = o.user_id
-    JOIN
-        order_items oi ON o.order_id = oi.order_id
-    JOIN
-        products p ON oi.product_id = p.id
-    WHERE
-        u.country = 'Germany'
-        AND u.gender = 'F'
-        AND p.brand = 'Calvin Klein'
-        AND o.status NOT IN ('Cancelled', 'Returned')
-    GROUP BY
-        u.id, u.first_name, u.last_name, u.gender, u.country, p.brand
-)
-SELECT
-    first_name,
-    last_name,
-    gender,
-    country,
-    brand,
-    total_revenue
-FROM
-    customer_revenue
-ORDER BY
-    total_revenue DESC
+USE capstone_db;
+SELECT u.first_name, u.last_name, u.gender, u.country, p.brand, SUM(o.num_of_item * oi.sale_price) AS total_sales_value
+FROM users u
+JOIN orders o ON u.id = o.user_id
+JOIN order_items oi ON o.order_id = oi.id
+JOIN products p ON oi.product_id = p.id
+WHERE 
+    u.country IN ('Germany') AND
+    u.gender IN ('F') AND	
+    p.brand IN ('Calvin Klein') AND
+    o.status NOT IN ('Cancelled', 'Returned')
+GROUP BY u.first_name, u.last_name, u.gender, u.country, p.brand
+ORDER BY total_sales_value DESC
 LIMIT 5;
